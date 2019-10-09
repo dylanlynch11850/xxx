ebddd225fe60f7999de7e240e9dfcc6ce33b2803
dylanlynch11850 <g00381344@gmit.ie>
2019-10-09 12:30:24 +0000

    week5

---
+++ exercise2.java
@@ -0,0 +1,15 @@
+//uncomment next line if program requires input
+//import java.util.Scanner;
+
+public class exercise2{
+        public static void main(String[] args){
+            //uncomment next line if program requires input
+            // Scanner input = new Scanner(System.in);
+
+            String name = "jim";
+            int number = 2;
+
+            System.out.println(name+" has "+number+" bikes.");
+
+        }
+}
