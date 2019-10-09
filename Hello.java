d3b358e58894dbacb4d31148b38f3961bb1c5aea
dylanlynch11850 <g00381344@gmit.ie>
2019-10-09 12:17:34 +0000

    week5

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+        public static void main(String[] args){
+            //uncomment next line if program requires input
+            // Scanner input = new Scanner(System.in);
+            System.out.println("fdtytyytyt");
+        }
+}
