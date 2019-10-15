91426ee58c8644c1a90f53a05222c2c7830f39cf
dylanlynch11850 <g00381344@gmit.ie>
2019-10-15 09:54:30 +0000

    week 6

---
+++ Areas.java
@@ -0,0 +1,37 @@
+//uncomment next line if program requires input
+//import java.util.Scanner;
+
+public class Areas{
+        public static void main(String[] args){
+            //uncomment next line if program requires input
+            // Scanner input = new Scanner(System.in);
+           int width = 0;
+           int area = 0;
+           int radius = 0;
+
+           system.out.println(area of square is:  "+area");
+
+
+
+
+           system.out.print("part 2: Area of rectangle");
+           system.out.print("enter  rectangle width:  ");
+           width = input.nextint()
+           area = lenght * width;
+           system.out.println(Area of rectangle is:  "+area");
+
+
+
+
+           system.out.print("part 3: Area of circle");
+           system.out.print("enter  circle radius:  ");
+           radius = input.nextInt();
+
+           area = lenght * width;
+           system.out.println(Area of rectangle is:  "+area");
+
+
+
+        }
+
+}
