fc584a9ffc3bc6e8c810153ce5640c2c27044a38
dylanlynch11850 <g00381344@gmit.ie>
2019-10-30 14:00:02 +0000

    revision week 8

---
+++ Hourglass.java
@@ -0,0 +1,24 @@
+//uncomment next line if program requires input
+//import java.util.Scanner;
+
+public class Hourglass{
+        public static void main(String[] args){
+            //uncomment next line if program requires input
+            //Scanner input = new Scanner(System.in);
+
+            System.out.println("      +++++++++++");
+            System.out.println("       +++++++++");
+            System.out.println("        +++++++");
+            System.out.println("         +++++");
+            System.out.println("          +++");
+            System.out.println("           +");
+            System.out.println("           +");
+            System.out.println("          +++");
+            System.out.println("         +++++");
+            System.out.println("        +++++++");
+            System.out.println("       +++++++++");
+            System.out.println("      +++++++++++");
+
+
+        }
+}
---
+++ Table.java
@@ -0,0 +1,18 @@
+//uncomment next line if program requires input
+//import java.util.Scanner;
+
+public class Table{
+        public static void main(String[] args){
+            //uncomment next line if program requires input
+            //Scanner input = new Scanner(System.in);
+
+         System.out.print("firstname\tSurname\n");
+         System.out.print("=================================\n");
+
+            System.out.print("Dylan\tLynch\n");
+                System.out.print("Dillion\tsLynch\n");
+
+
+
+        }
+}
